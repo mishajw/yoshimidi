3a7289c9db9746b4d3eb3c25bfe3e59c7f34e5c4
Misha Wagner <mishajw@gmail.com>
2023-07-17 21:10:18 +0100

    Fix parsed dataset path.

--- yoshimidi/output_config.py
+++ yoshimidi/output_config.py
@@ -10,7 +10,7 @@ class OutputConfig(BaseModel, extra="forbid"):
     root: Path = Path("out")
 
     dataset_dir: Path = root / "dataset"
-    dataset_parsed: Path = dataset_dir / "dataset_parsed"
+    dataset_parsed: Path = dataset_dir / "dataset_parsed.jsonl"
     dataset_raw: Path = dataset_dir / "dataset_raw"
     dataset_raw_compressed: Path = dataset_dir / "dataset_raw.tar.gz"
 
